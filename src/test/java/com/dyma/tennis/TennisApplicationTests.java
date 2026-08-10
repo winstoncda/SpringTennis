2181630d18a5f3a45a9712c5469a1351ba67c99c
winstoncda <olivier.pro.cda@gmail.com>
2026-08-10 21:06:17 +0200

    modification du test automatisé'

--- src/test/java/com/dyma/tennis/TennisApplicationTests.java
+++ src/test/java/com/dyma/tennis/TennisApplicationTests.java
@@ -1,13 +1,30 @@
 package com.dyma.tennis;
 
+import com.dyma.tennis.repository.HealthCheckRepository;
+import com.dyma.tennis.rest.HealthCheckController;
+import com.dyma.tennis.service.HealthCheckService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TennisApplicationTests {
 
+	@Autowired
+	private HealthCheckController healthCheckController;
+
+	@Autowired
+	private HealthCheckService healthCheckService;
+
+	@Autowired
+	private HealthCheckRepository healthCheckRepository;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(healthCheckController).isNotNull();
+		Assertions.assertThat(healthCheckService).isNotNull();
+		Assertions.assertThat(healthCheckRepository).isNotNull();
 	}
 
 }
