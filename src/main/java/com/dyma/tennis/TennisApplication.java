2ee176fa72e1a4cdc107ebfa68926b4f80e222ef
winstoncda <olivier.pro.cda@gmail.com>
2026-08-10 10:28:43 +0200

    mise en place bdd avec docker et ajout de la couche repository

--- src/main/java/com/dyma/tennis/TennisApplication.java
+++ src/main/java/com/dyma/tennis/TennisApplication.java
@@ -2,9 +2,8 @@ package com.dyma.tennis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class TennisApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dyma/tennis/service/HealthCheckService.java
+++ src/main/java/com/dyma/tennis/service/HealthCheckService.java
@@ -1,13 +1,23 @@
 package com.dyma.tennis.service;
 
-import org.springframework.stereotype.Service;
-
 import com.dyma.tennis.ApplicationStatus;
 import com.dyma.tennis.HealthCheck;
+import com.dyma.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
+
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "Welcome to the Tennis application!");
+        Long applicationConnections = healthCheckRepository.countApplicationConnections();
+        if (applicationConnections > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "Welcome to Dyma Tennis!");
+        } else {
+            return new HealthCheck(ApplicationStatus.KO, "Dyma Tennis is not fully functional, please check your configuration.");
+        }
     }
 }
