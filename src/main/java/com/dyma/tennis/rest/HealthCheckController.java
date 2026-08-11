b6a22ee58bceb1a8778a6ed3af0916d808f55041
winstoncda <olivier.pro.cda@gmail.com>
2026-08-11 09:42:00 +0200

    mise en place de swagger

--- src/main/java/com/dyma/tennis/rest/HealthCheckController.java
+++ src/main/java/com/dyma/tennis/rest/HealthCheckController.java
@@ -1,21 +1,33 @@
 package com.dyma.tennis.rest;
 
+import com.dyma.tennis.HealthCheck;
+import com.dyma.tennis.service.HealthCheckService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dyma.tennis.HealthCheck;
-import com.dyma.tennis.service.HealthCheckService;
-
+@Tag(name = "HealthCheck API")
 @RestController
 public class HealthCheckController {
 
     @Autowired
     private HealthCheckService healthCheckService;
 
+    @Operation(summary = "Returns application status", description = "Returns the application status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Healthcheck status with some details",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HealthCheck.class))})
+
+    })
     @GetMapping("/healthcheck")
-    public HealthCheck healthCheck() {
+    public HealthCheck healthcheck() {
         return healthCheckService.healthCheck();
     }
-
 }
